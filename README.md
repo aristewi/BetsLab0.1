c1b96359ba5710c288f7d51c12963502fe919ced
aaristegui <39808834+aristewi@users.noreply.github.com>
2021-09-11 00:39:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BetsLab0.1
+ Laboratorio prestado de IS1, para realizar  el analisis del codigo y corrección de code smells
---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"gui","c":"PostontziaGUI","l":"PostontziaGUI(BezeroaGUI, Bezeroa)","u":"%3Cinit%3E(gui.BezeroaGUI,domain.Bezeroa)"}];updateSearchResults();
